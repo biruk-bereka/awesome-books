8f1b6d6232f54e0bb07498e2b522c484ff29a5fa
biruk-bereka <birukbeletech1212@gmail.com>
2023-01-10 00:29:25 +0300

    Add a function to add new books

--- index.js
+++ index.js
@@ -1,42 +1,60 @@
-const books = [
-    {
-        title: "First Book",
-        author: "John Doe"
-    },
+let booksCollection = [];
 
-    {
-        title: "Second Book",
-        author: "John Doe"
-    }
-];
+const setBooks = (books) => {
+  localStorage.setItem("Books", JSON.stringify(books));
+};
 
-const booksContainer = document.querySelector('.books');
+const getBooks = () => {
+  const books = JSON.parse(localStorage.getItem("Books"));
+  if (books) return books;
+  return [];
+};
 
-function displayBook(book) {
-    const bookContainer = document.createElement('div');
-    const title = document.createElement('p');
+const updatedBooksStorge = (book) => {
+  booksCollection = getBooks();
+  booksCollection.push(book);
+  setBooks(booksCollection);
+};
+
+const displayBook = () => {
+  const books = getBooks();
+  const booksContainer = document.querySelector(".books");
+  booksContainer.innerHTML = "";
+  books.forEach((book,index) => {
+    const bookContainer = document.createElement("div");
+    const title = document.createElement("p");
     title.innerText = book.title;
-    const author = document.createElement('p');
+    const author = document.createElement("p");
     author.innerText = book.author;
-    const removeButton = document.createElement('button');
-    removeButton.className = 'remove';
-    removeButton.innerText = 'Remove';
+    const removeButton = document.createElement("button");
+    removeButton.className = "remove";
+    removeButton.type = "button";
+    removeButton.onclick = () => deleteBook(index);
+    removeButton.innerText = "Remove";
     bookContainer.append(title, author, removeButton);
     booksContainer.append(bookContainer);
+  });
+};
+
+const addBook = () => {
+  const bookTitle = document.getElementById("title").value;
+  const bookAuthor = document.getElementById("author").value;
+  let book = {
+    title: document.getElementById("title").value,
+    author: document.getElementById("author").value,
+  };
+  updatedBooksStorge(book);
+  displayBook();
+  bookTitle.value = "";
+  bookAuthor.value = "";
 }
 
-books.forEach(book => displayBook(book));
+const deleteBook = (bookIndex) => {}
 
+const form = document.querySelector(".form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addBook();
+});
 
-function capture() {
-    let book = {
-        title : document.getElementById('title').value,
-        author : document.getElementById('author').value,
-        id : Date.now(),
-    }
-    books.push(book);
-    displayBook(book);
-}
-
-
-
+displayBook();
